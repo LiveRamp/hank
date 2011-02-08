769a463413b82c3e1b43c6cf0762414eff799475
bryanduxbury <bryan@rapleaf.com>
2011-02-07 17:10:18 -0800

    tests for all possible messages, clean up the test some more (many fewer copy/pasted lines)

--- test/java/com/rapleaf/hank/update_daemon/TestUpdateDaemon.java
+++ test/java/com/rapleaf/hank/update_daemon/TestUpdateDaemon.java
@@ -17,6 +17,7 @@ package com.rapleaf.hank.update_daemon;
 
 import java.net.UnknownHostException;
 import java.util.Collections;
+import java.util.EnumSet;
 import java.util.Set;
 
 import junit.framework.TestCase;
@@ -52,6 +53,15 @@ public class TestUpdateDaemon extends TestCase {
     Logger.getRootLogger().setLevel(Level.ALL);
   }
 
+  private static final RingConfig mockRingConfig = new MockRingConfig(null, null, 0, null) {
+    @Override
+    public Set<Integer> getDomainPartitionsForHost(
+        PartDaemonAddress hostAndPort, int domainId)
+    throws DataNotFoundException {
+      return Collections.singleton(0);
+    }
+  };
+
   public void testColdStart() throws Exception {
     final MockUpdater mockUpdater = new MockUpdater();
 
@@ -62,35 +72,7 @@ public class TestUpdateDaemon extends TestCase {
       }
     };
 
-    final RingConfig mockRingConfig = new MockRingConfig(null, null, 0, null) {
-      @Override
-      public Set<Integer> getDomainPartitionsForHost(
-          PartDaemonAddress hostAndPort, int domainId)
-      throws DataNotFoundException {
-        return Collections.singleton(0);
-      }
-    };
-
-    DomainGroupConfig mockDomainGroupConfig = new MockDomainGroupConfig("myDomainGroup") {
-      @Override
-      public String getName() {
-        return "myDomainGroup";
-      }
-
-      @Override
-      public DomainGroupConfigVersion getLatestVersion() {
-        return new MockDomainGroupConfigVersion(Collections.singleton(
-            (DomainConfigVersion)new MockDomainConfigVersion(
-                new MockDomainConfig("myDomain",
-                    1,
-                    new ConstantPartitioner(),
-                    mockStorageEngine,
-                    0),
-                0)),
-            null,
-            0);
-      }
-    };
+    DomainGroupConfig mockDomainGroupConfig = getMockDomainGroupConfig(mockStorageEngine);
 
     final RingGroupConfig mockRingGroupConfig = new MockRingGroupConfig(mockDomainGroupConfig, "myRingGroup", null) {
       @Override
@@ -144,35 +126,7 @@ public class TestUpdateDaemon extends TestCase {
       }
     };
 
-    final RingConfig mockRingConfig = new MockRingConfig(null, null, 0, null) {
-      @Override
-      public Set<Integer> getDomainPartitionsForHost(
-          PartDaemonAddress hostAndPort, int domainId)
-      throws DataNotFoundException {
-        return Collections.singleton(0);
-      }
-    };
-
-    DomainGroupConfig mockDomainGroupConfig = new MockDomainGroupConfig("myDomainGroup") {
-      @Override
-      public String getName() {
-        return "myDomainGroup";
-      }
-
-      @Override
-      public DomainGroupConfigVersion getLatestVersion() {
-        return new MockDomainGroupConfigVersion(Collections.singleton(
-            (DomainConfigVersion)new MockDomainConfigVersion(
-                new MockDomainConfig("myDomain",
-                    1,
-                    new ConstantPartitioner(),
-                    mockStorageEngine,
-                    0),
-                0)),
-            null,
-            0);
-      }
-    };
+    DomainGroupConfig mockDomainGroupConfig = getMockDomainGroupConfig(mockStorageEngine);
 
     final RingGroupConfig mockRingGroupConfig = new MockRingGroupConfig(mockDomainGroupConfig, "myRingGroup", null) {
       @Override
@@ -224,5 +178,86 @@ public class TestUpdateDaemon extends TestCase {
         DaemonState.IDLE,
         mockCoordinator.getDaemonState(null, 0, new PartDaemonAddress("localhost", 12345), null));
     assertTrue("update() was called on the storage engine", mockUpdater.isUpdated());
+  }
+  
+  public void testDoesNothingOnOtherEvents() throws Exception {
+    final MockUpdater mockUpdater = new MockUpdater();
+
+    final StorageEngine mockStorageEngine = new MockStorageEngine() {
+      @Override
+      public Updater getUpdater(UpdateDaemonConfigurator configurator, int partNum) {
+        return mockUpdater;
+      }
+    };
+
+    DomainGroupConfig mockDomainGroupConfig = getMockDomainGroupConfig(mockStorageEngine);
+
+    final RingGroupConfig mockRingGroupConfig = new MockRingGroupConfig(mockDomainGroupConfig, "myRingGroup", null) {
+      @Override
+      public RingConfig getRingConfigForHost(PartDaemonAddress hostAddress)
+          throws DataNotFoundException {
+        return mockRingConfig;
+      }
+    };
+
+    MockCoordinator mockCoordinator = new MockCoordinator() {
+      private DaemonState daemonState;
+
+      @Override
+      public DaemonState getDaemonState(String ringGroupName, int ringNumber,
+          PartDaemonAddress hostAddress, DaemonType type) {
+        return daemonState;
+      }
+
+      @Override
+      public RingGroupConfig getRingGroupConfig(String ringGroupName)
+          throws DataNotFoundException {
+        return mockRingGroupConfig;
+      }
+
+      @Override
+      public void setDaemonState(String ringGroupName, int ringNumber,
+          PartDaemonAddress hostAddress, DaemonType type, DaemonState state) {
+        daemonState = state;
+      }
+    };
+
+    MockUpdateDaemonConfigurator mockConfigurator = new MockUpdateDaemonConfigurator(1, null, 12345, mockCoordinator, "myRingGroup", 1);
+
+    UpdateDaemon ud = new UpdateDaemon(mockConfigurator, "localhost");
+
+    for (DaemonState state : EnumSet.complementOf(EnumSet.of(DaemonState.UPDATEABLE, DaemonState.UPDATING))) {
+      ud.onDaemonStateChange(null, 0, null, null, state);
+      assertEquals("Daemon is unchanged after being probed with " + state,
+          null,
+          mockCoordinator.getDaemonState(null, 0, new PartDaemonAddress("localhost", 12345), null));
+      assertFalse("update() was not called on the storage engine", mockUpdater.isUpdated());
+    }
+  }
+
+  private static DomainGroupConfigVersion getMockDomainGroupConfigVersion(
+      final StorageEngine mockStorageEngine) 
+  {
+    return new MockDomainGroupConfigVersion(Collections.singleton(
+        (DomainConfigVersion)new MockDomainConfigVersion(
+            new MockDomainConfig("myDomain",
+                1,
+                new ConstantPartitioner(),
+                mockStorageEngine,
+                0),
+            0)),
+        null,
+        0);
+  }
+
+  private DomainGroupConfig getMockDomainGroupConfig(
+      final StorageEngine mockStorageEngine) {
+    DomainGroupConfig mockDomainGroupConfig = new MockDomainGroupConfig("myDomainGroup") {
+      @Override
+      public DomainGroupConfigVersion getLatestVersion() {
+        return getMockDomainGroupConfigVersion(mockStorageEngine);
+      }
+    };
+    return mockDomainGroupConfig;
   }
 }
