0b61c6d021aea3bd2a97f83ccdbda4578f6fcc55
bryanduxbury <bryan@rapleaf.com>
2011-02-18 20:29:43 -0800

    add stub of integration test

---
+++ test/java/com/rapleaf/hank/IntegrationTest.java
@@ -0,0 +1,37 @@
+package com.rapleaf.hank;
+
+public class IntegrationTest extends ZkTestCase {
+
+  public IntegrationTest() throws Exception {
+    super();
+  }
+
+  public void testItAll() throws Exception {
+    // initialize the bare structure
+    
+    // use cli tools to create a pair of domains
+    
+    // write a base version of each domain
+    
+    // launch 2x 2-node rings
+    
+    // launch the data deployer
+    
+    // launch a smart client server
+    
+    // open a dumb client (through the smart client)
+    
+    // make a few requests
+    
+    // push a new version of one of the domains
+    
+    // keep making requests
+    
+    // take down one of the nodes in one of the rings "unexpectedly"
+    
+    // keep making requests
+    
+    // shut it all down
+  }
+
+}
