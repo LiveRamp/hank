ae7d76112253a95671c391c928aeab21317c8fce
bryanduxbury <bryan@rapleaf.com>
2011-03-03 11:52:36 -0800

    git rid of now-redundant PartDaemon and UpdateDaemon state enums

--- src/java/com/rapleaf/hank/coordinator/PartDaemonState.java
+++
@@ -1,5 +0,0 @@
-package com.rapleaf.hank.coordinator;
-
-public enum PartDaemonState {
-  IDLE, STARTABLE, STARTING, STARTED, STOPPABLE, STOPPING;
-}
--- src/java/com/rapleaf/hank/coordinator/UpdateDaemonState.java
+++
@@ -1,5 +0,0 @@
-package com.rapleaf.hank.coordinator;
-
-public enum UpdateDaemonState {
-  IDLE, UPDATABLE, UPDATING;
-}
