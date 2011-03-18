456d19c5aeb4aac2e77c8856d5d459bd3e441956
bryanduxbury <bryan@rapleaf.com>
2011-03-18 12:22:04 -0700

    clear a few minor warnings in test, add license to new files

--- test/java/com/rapleaf/hank/storage/cueball/TestCueballFactory.java
+++ test/java/com/rapleaf/hank/storage/cueball/TestCueballFactory.java
@@ -1,3 +1,18 @@
+/**
+ *  Copyright 2011 Rapleaf
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.rapleaf.hank.storage.cueball;
 
 import java.util.HashMap;
@@ -7,7 +22,6 @@ import org.apache.log4j.Logger;
 
 import com.rapleaf.hank.BaseTestCase;
 import com.rapleaf.hank.hasher.Murmur64Hasher;
-import com.rapleaf.hank.storage.StorageEngine;
 
 public class TestCueballFactory extends BaseTestCase {
   private static final Logger LOG = Logger.getLogger(TestCueballFactory.class);
@@ -34,6 +48,6 @@ public class TestCueballFactory extends BaseTestCase {
     options.put(Cueball.Factory.READ_BUFFER_BYTES_KEY, 32000);
     options.put(Cueball.Factory.REMOTE_DOMAIN_ROOT_KEY, "/path/to/what/i/say");
     options.put(Cueball.Factory.VALUE_SIZE_KEY, 15);
-    StorageEngine storageEngine = factory.getStorageEngine(options, "domain0");
+    factory.getStorageEngine(options, "domain0");
   }
 }
