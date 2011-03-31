c2a0ab32d1e6f05b594eac48e57bae1d8be80825
bryanduxbury <bryan@rapleaf.com>
2011-03-31 11:42:32 -0700

    stub of tests for Footer

---
+++ test/java/com/rapleaf/hank/storage/cueball/TestFooter.java
@@ -0,0 +1,13 @@
+package com.rapleaf.hank.storage.cueball;
+
+import com.rapleaf.hank.BaseTestCase;
+
+public class TestFooter extends BaseTestCase {
+  public void testValid() throws Exception {
+    fail();
+  }
+
+  public void testInvalid() throws Exception {
+    fail();
+  }
+}
