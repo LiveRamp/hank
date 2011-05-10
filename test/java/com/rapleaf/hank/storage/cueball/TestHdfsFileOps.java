392e6be430bf2f9677c23f2f79392bb4fcb5a4bf
bryanduxbury <bryan@rapleaf.com>
2011-05-10 14:06:57 -0700

    first draft of HdfsFileOps, not-yet-implemented test for same

---
+++ test/java/com/rapleaf/hank/storage/cueball/TestHdfsFileOps.java
@@ -0,0 +1,9 @@
+package com.rapleaf.hank.storage.cueball;
+
+import com.rapleaf.hank.BaseTestCase;
+
+public class TestHdfsFileOps extends BaseTestCase {
+  public void testIt() throws Exception {
+    fail();
+  }
+}
