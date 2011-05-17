4f2f4120d06413107be320c77a04f51819766ad7
bryanduxbury <bryan@rapleaf.com>
2011-05-17 15:28:54 -0700

    implement the closedAt method on ZkDomainVersionConfig

--- src/java/com/rapleaf/hank/coordinator/zk/ZkDomainVersionConfig.java
+++ src/java/com/rapleaf/hank/coordinator/zk/ZkDomainVersionConfig.java
@@ -1,25 +1,25 @@
 package com.rapleaf.hank.coordinator.zk;
 
-import org.apache.commons.lang.NotImplementedException;
+import org.apache.zookeeper.KeeperException;
 
 import com.rapleaf.hank.coordinator.DomainVersionConfig;
 import com.rapleaf.hank.zookeeper.ZooKeeperPlus;
 
 public class ZkDomainVersionConfig implements DomainVersionConfig {
-  private final ZooKeeperPlus zk;
   private final int versionNumber;
+  private final long closedAt;
 
-  public ZkDomainVersionConfig(ZooKeeperPlus zk, String path) {
-    this.zk = zk;
+  public ZkDomainVersionConfig(ZooKeeperPlus zk, String path) throws KeeperException, InterruptedException {
     String[] toks = path.split("/");
     String last = toks[toks.length-1];
     toks = last.split("_");
     this.versionNumber = Integer.parseInt(toks[1]);
+    this.closedAt = zk.exists(path, false).getCtime();
   }
 
   @Override
   public long getClosedAt() {
-    throw new NotImplementedException();
+    return closedAt;
   }
 
   @Override
