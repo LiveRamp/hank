82103760aca4a15f33a91dafde07bf3cd1b13246
bryanduxbury <bryan@rapleaf.com>
2011-05-26 11:25:48 -0700

    git rid of the in_memory coordinator impl

--- test/java/com/rapleaf/hank/coordinator/in_memory/MemDomainVersion.java
+++
@@ -1,9 +0,0 @@
-package com.rapleaf.hank.coordinator.in_memory;
-
-import com.rapleaf.hank.coordinator.mock.MockDomainVersion;
-
-public class MemDomainVersion extends MockDomainVersion {
-  public MemDomainVersion(int versionNumber, Long closedAt) {
-    super(versionNumber, closedAt);
-  }
-}
