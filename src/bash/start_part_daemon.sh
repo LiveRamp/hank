2bf355a87fb08b981b99877213261ed43854be68
bryanduxbury <bryan@rapleaf.com>
2011-06-14 16:27:52 -0700

    -write daemonization script output to part_daemon.out instead of, well, nowhere - fixed #78 - you can now create bin/part_daemon_extra.sh to inject some stuff into the startup

--- src/bash/start_part_daemon.sh
+++ src/bash/start_part_daemon.sh
@@ -4,4 +4,9 @@ BINDIR=`dirname "$0"`
 
 . "$BINDIR"/env.sh
 
-java -cp "$CLASSPATH" com.rapleaf.hank.part_daemon.PartDaemonServer $@
+if [ -f "$BINDIR"/part_daemon_extra.sh ]; then
+  echo "Loading extra part daemon config from " "$BINDIR"/part_daemon_extra.sh
+  . "$BINDIR"/part_daemon_extra.sh
+fi
+
+java $EXTRA_JVM_ARGS -cp "$CLASSPATH" com.rapleaf.hank.part_daemon.PartDaemonServer $@
