4901e1d9b984834f44a146a867a8eecc3fe0703b
Gareth Aye <gareth@rapleafs-macbook-pro-133.local>
2011-06-21 15:05:21 -0700

    Tests for curly and cueball deleters

---
+++ test/java/com/rapleaf/hank/storage/cueball/TestCueballDeleter.java
@@ -0,0 +1,23 @@
+package com.rapleaf.hank.storage.cueball;
+
+import java.io.File;
+
+public class TestCueballDeleter extends AbstractCueballTest {
+  private final String LOCAL_ROOT = localTmpDir + "/local";
+
+  public void setUp() throws Exception {
+    super.setUp();
+    new File(LOCAL_ROOT).mkdirs();
+  }
+  
+  public void testDeleter() throws Exception {
+    assertTrue(localFileExists(LOCAL_ROOT));
+    CueballDeleter deleter = new CueballDeleter(LOCAL_ROOT);
+    deleter.delete();
+    assertFalse(localFileExists(LOCAL_ROOT));
+  }
+  
+  private boolean localFileExists(String localFile) {
+    return new File(localFile).exists();
+  }
+}
