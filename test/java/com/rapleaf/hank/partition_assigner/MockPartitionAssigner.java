9c50612c42611d1e83cde773c4c60f53f3149337
Gareth Aye <gareth@rapleafs-macbook-pro-133.local>
2011-06-29 13:10:27 -0700

    Adding MockPartitionAssigner

--- test/java/com/rapleaf/hank/partition_assigner/MockPartitionAssigner.java
+++ test/java/com/rapleaf/hank/partition_assigner/MockPartitionAssigner.java
@@ -1,6 +1,17 @@
 package com.rapleaf.hank.partition_assigner;
 
+import java.io.IOException;
 
-public class MockPartitionAssigner {
+import com.rapleaf.hank.coordinator.Domain;
+import com.rapleaf.hank.coordinator.RingGroup;
+
+
+public class MockPartitionAssigner implements PartitionAssigner {
+
+  @Override
+  public void assign(RingGroup ringGroup, int ringNum, Domain domain)
+      throws IOException {
+    // TODO Auto-generated method stub
+  }
   
 }
