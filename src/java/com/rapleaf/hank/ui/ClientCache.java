54e7dc564dc67dc9f9261e238fcd8cab2865da29
Thomas Kielbus <thomas@rapleaf.com>
2011-08-04 15:46:04 -0700

    Minor variable renaming.

--- src/java/com/rapleaf/hank/ui/ClientCache.java
+++ src/java/com/rapleaf/hank/ui/ClientCache.java
@@ -1,30 +1,29 @@
 package com.rapleaf.hank.ui;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.thrift.TException;
-
 import com.rapleaf.hank.client.HankSmartClient;
 import com.rapleaf.hank.coordinator.Coordinator;
 import com.rapleaf.hank.coordinator.RingGroup;
 import com.rapleaf.hank.generated.SmartClient;
 import com.rapleaf.hank.generated.SmartClient.Iface;
+import org.apache.thrift.TException;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public final class ClientCache implements IClientCache {
   private static final Map<String, SmartClient.Iface> cachedClients = new HashMap<String, Iface>();
   private final Coordinator coord;
 
-  public ClientCache(Coordinator coord) {
-    this.coord = coord;
+  public ClientCache(Coordinator coordinator) {
+    this.coord = coordinator;
   }
 
-  public synchronized SmartClient.Iface getSmartClient(RingGroup rgc) throws IOException, TException {
-    Iface c = cachedClients.get(rgc.getName());
+  public synchronized SmartClient.Iface getSmartClient(RingGroup ringGroup) throws IOException, TException {
+    Iface c = cachedClients.get(ringGroup.getName());
     if (c == null) {
-      c = new HankSmartClient(coord, rgc.getName(), 1);
-      cachedClients.put(rgc.getName(), c);
+      c = new HankSmartClient(coord, ringGroup.getName(), 1);
+      cachedClients.put(ringGroup.getName(), c);
     }
     return c;
   }
