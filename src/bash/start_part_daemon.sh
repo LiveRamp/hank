73208ac3d95e996fa0966667785efd8417b19c49
Thomas Kielbus <thomas@rapleaf.com>
2011-08-16 16:46:32 -0700

    Revert "Extra part daemon script can be specified on the command line instead"
    This reverts commit 0fbf909cd5cd777753d9d9fa05a767fe259ffbd6.

--- src/bash/start_part_daemon.sh
+++ src/bash/start_part_daemon.sh
@@ -4,9 +4,9 @@ BINDIR=`dirname "$0"`
 
 . "$BINDIR"/env.sh
 
-if [ $# -eq 1 ]; then
-  EXTRA_CONFIGURATION_SCRIPT=$1
-  . $EXTRA_CONFIGURATION_SCRIPT
+if [ -f "$BINDIR"/part_daemon_extra.sh ]; then
+  echo "Loading extra part daemon config from " "$BINDIR"/part_daemon_extra.sh
+  . "$BINDIR"/part_daemon_extra.sh
 fi
 
 java $EXTRA_JVM_ARGS -cp "$CLASSPATH" com.rapleaf.hank.part_daemon.PartDaemonServer $@
