5eb68e25a433b89203375db866e7bd3fcf570ed1
Thomas Kielbus <thomas@rapleaf.com>
2011-08-25 13:06:46 -0700

    PartitionServer can start even when some partitions have no current version.

--- src/java/com/rapleaf/hank/partition_server/DomainAccessor.java
+++ src/java/com/rapleaf/hank/partition_server/DomainAccessor.java
@@ -61,17 +61,17 @@ class DomainAccessor {
    */
   public boolean get(ByteBuffer key, Result result) throws IOException {
     int partition = partitioner.partition(key, partitionAccessors.length);
-    PartitionAccessor currentPRC = partitionAccessors[partition];
-    if (currentPRC == null) {
+    PartitionAccessor partitionAccessor = partitionAccessors[partition];
+    if (partitionAccessor == null) {
       return false;
     }
     // Increment requests counter
-    currentPRC.getRequests().incrementAndGet();
+    partitionAccessor.getRequests().incrementAndGet();
     // Perform get()
-    currentPRC.getReader().get(key, result);
+    partitionAccessor.getReader().get(key, result);
     if (result.isFound()) {
       // Increment hits counter
-      currentPRC.getHits().incrementAndGet();
+      partitionAccessor.getHits().incrementAndGet();
     }
     return true;
   }
