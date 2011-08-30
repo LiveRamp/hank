ffd8b258b654c4fe6a83cb31a5b8dcc50ec580fd
Thomas Kielbus <thomas@rapleaf.com>
2011-08-30 15:48:02 -0700

    Fix build errors in MockDomain

--- src/java/com/rapleaf/hank/coordinator/mock/MockDomain.java
+++ src/java/com/rapleaf/hank/coordinator/mock/MockDomain.java
@@ -15,19 +15,20 @@
  */
 package com.rapleaf.hank.coordinator.mock;
 
-import java.io.IOException;
-import java.util.Map;
-import java.util.SortedSet;
-import java.util.TreeSet;
-
 import com.rapleaf.hank.coordinator.AbstractDomain;
 import com.rapleaf.hank.coordinator.DomainVersion;
 import com.rapleaf.hank.partitioner.Partitioner;
 import com.rapleaf.hank.storage.StorageEngine;
 import com.rapleaf.hank.storage.StorageEngineFactory;
 
+import java.io.IOException;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class MockDomain extends AbstractDomain {
   private final String name;
+  private final int id;
   private final int numParts;
   private final Partitioner part;
   private final StorageEngine storageEngine;
@@ -35,12 +36,14 @@ public class MockDomain extends AbstractDomain {
   private final Map<String, Object> storageEngineOptions;
 
   public MockDomain(String name,
+                    int id,
                     int numParts,
                     Partitioner part,
                     StorageEngine storageEngine,
                     Map<String, Object> storageEngineOptions,
                     DomainVersion version) {
     this.name = name;
+    this.id = id;
     this.numParts = numParts;
     this.part = part;
     this.storageEngine = storageEngine;
@@ -49,11 +52,15 @@ public class MockDomain extends AbstractDomain {
   }
 
   public MockDomain(String domainName) {
-    this(domainName, 1, null, null, null, null);
+    this(domainName, 0, 1, null, null, null, null);
   }
 
   public String getName() {
     return name;
+  }
+
+  public int getId() {
+    return id;
   }
 
   public int getNumParts() {
