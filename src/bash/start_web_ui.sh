12be702dc38dde490ec5127212ffb2b65483e92b
bryanduxbury <bryan@rapleaf.com>
2011-09-30 10:30:11 -0700

    give the web ui an 'extra' script for config during startup

--- src/bash/start_web_ui.sh
+++ src/bash/start_web_ui.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 BINDIR=`dirname "$0"`
+CONFDIR=`dirname "$0"`/../conf
 
 . "$BINDIR"/env.sh
 
-java -cp "$CLASSPATH" com.rapleaf.hank.ui.WebUiServer $@
+if [ -f "$CONFDIR"/web_ui_extra.sh ]; then
+  echo "Loading extra partition server config from " "$CONFDIR"/web_ui_extra.sh
+  . "$CONFDIR"/web_ui_extra.sh
+fi
+
+java $EXTRA_JVM_ARGS -cp "$CLASSPATH" com.rapleaf.hank.ui.WebUiServer $@
