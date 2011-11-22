be3fe70432e09bb1ae54cb77916485d1e960a6c7
Thomas Kielbus <thomas@rapleaf.com>
2011-11-21 18:09:51 -0800

    Add extensive Javadoc explanation of the strategy used to select which Host connection to use when performing a query.

--- src/java/com/rapleaf/hank/client/HostConnectionPool.java
+++ src/java/com/rapleaf/hank/client/HostConnectionPool.java
@@ -26,6 +26,40 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.*;
 
+/**
+ * HostConnectionPool manages a collection of connections to Hosts. For a given
+ * partition, there might be multiple Hosts serving it, and there might be
+ * multiple established connections to each of these Hosts. This class
+ * implements the strategy used to select which connection to use when
+ * performing a query, and takes care of managing retries when queries fail.
+ *
+ * The strategy is as follows:
+ *
+ * Connections are organized by Host (the server they correspond to). The list
+ * of connections to a Host is initially randomized, so that different
+ * HostConnectionPool instances will attempt to use connections in a different
+ * order.
+ *
+ * HostConnectionPool maintains an internal indicator of what Host was used
+ * last by any query. To distribute load, the next query will attempt to
+ * connect to the next Host, and so on. Note that initially, this Host iterator
+ * is randomized.
+ *
+ * When performing a query, HostConnectionPool first loops over all hosts and
+ * connections (starting from the last used host iterator) looking for an
+ * unused connection. An unused connection is a connection that no client is
+ * waiting on. In other words, a connection that is not locked. If such a
+ * connection is found, it will be the one used to perform the query.
+ * Otherwise, HostConnectionPool loops over all hosts again, looking for a
+ * random available connection (one for which the Host is serving) to use. If
+ * it cannot, an error is returned.
+ *
+ * When the connection to use has been determined, the query is performed. In
+ * case of failure, HostConnectionPool will re-attempt a given number of times,
+ * each time determining a new connection to use as described earlier. (And
+ * using a local Host iterator.)
+ *
+ */
 public class HostConnectionPool {
 
   private static Logger LOG = Logger.getLogger(HostConnectionPool.class);
