23fde2c03a9029359f8396cf7c0218286f63882e
Thomas Kielbus <thomas@rapleaf.com>
2012-01-12 15:22:48 -0800

    Fix logic in ModPartitionAssigner

--- src/java/com/rapleaf/hank/partition_assigner/ModPartitionAssigner.java
+++ src/java/com/rapleaf/hank/partition_assigner/ModPartitionAssigner.java
@@ -42,28 +42,36 @@ public class ModPartitionAssigner implements PartitionAssigner {
     for (DomainGroupVersionDomainVersion dgvdv : domainGroupVersion.getDomainVersions()) {
       Domain domain = dgvdv.getDomain();
       for (Host host : ring.getHosts()) {
+        Set<Integer> partitionMappings = null;
         Map<Domain, Set<Integer>> domainToPartitionsMappings = hostToDomainToPartitionsMappings.get(host);
         if (domainToPartitionsMappings != null) {
-          Set<Integer> partitionsMapping = domainToPartitionsMappings.get(domain);
-          if (partitionsMapping != null) {
-            HostDomain hostDomain = host.getHostDomain(domain);
-            // If domain is not assigned to host, ring is not assigned
-            if (hostDomain == null) {
-              return false;
-            }
-            Set<HostDomainPartition> partitions = hostDomain.getPartitions();
-            if (partitions.size() != partitionsMapping.size()) {
-              // If partitions assigned to host and required partition mapping differ in size, ring is not assigned
-              return false;
-            }
+          partitionMappings = domainToPartitionsMappings.get(domain);
+        }
 
-            // Check that all required partition mappings are satisfied
-            for (Integer partitionMapping : partitionsMapping) {
-              HostDomainPartition partition = hostDomain.getPartitionByNumber(partitionMapping);
-              // If the partition is not assigned, or it is deletable, the mapping is not satisfied
-              if (partition == null || partition.isDeletable()) {
-                return false;
-              }
+        HostDomain hostDomain = host.getHostDomain(domain);
+
+        // Check for extra mappings
+        if (hostDomain != null) {
+          for (HostDomainPartition partition : hostDomain.getPartitions()) {
+            if (!partition.isDeletable() &&
+                (partitionMappings == null ||
+                    !partitionMappings.contains(partition.getPartitionNumber()))) {
+              return false;
+            }
+          }
+        }
+
+        // Check for missing mappings
+        if (partitionMappings != null) {
+          // Check that domain is assigned
+          if (hostDomain == null) {
+            return false;
+          }
+          for (Integer partitionMapping : partitionMappings) {
+            // Check that partition is assigned
+            HostDomainPartition partition = hostDomain.getPartitionByNumber(partitionMapping);
+            if (partition == null || partition.isDeletable()) {
+              return false;
             }
           }
         }
@@ -93,8 +101,9 @@ public class ModPartitionAssigner implements PartitionAssigner {
         // Delete extra mappings
         if (hostDomain != null) {
           for (HostDomainPartition partition : hostDomain.getPartitions()) {
-            if (partitionMappings == null ||
-                !partitionMappings.contains(partition.getPartitionNumber())) {
+            if (!partition.isDeletable() &&
+                (partitionMappings == null ||
+                    !partitionMappings.contains(partition.getPartitionNumber()))) {
               partition.setDeletable(true);
             }
           }
