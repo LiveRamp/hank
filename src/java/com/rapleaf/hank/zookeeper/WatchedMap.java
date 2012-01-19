0fc53c891848228325eb235d7254a1099ba8f827
Thomas Kielbus <thomas@rapleaf.com>
2012-01-19 14:14:44 -0800

    Make it impossible to put in a WatchedMap

--- src/java/com/rapleaf/hank/zookeeper/WatchedMap.java
+++ src/java/com/rapleaf/hank/zookeeper/WatchedMap.java
@@ -240,6 +240,6 @@ public class WatchedMap<T> extends AbstractMap<String, T> {
 
   @Override
   public T put(String key, T value) {
-    return internalMap.put(key, value);
+    throw new UnsupportedOperationException("The put operation is not supported by WatchedMap.");
   }
 }
