e9ef69dbd72cdad8027855d59afe503e46a22209
Michel Tricot <michel@rapleaf.com>
2012-02-22 19:48:51 -0800

    Add a new notifier type to notify in SQL database

--- src/java/com/rapleaf/hank/monitor/notification/SQLNotificationFormatter.java
+++ src/java/com/rapleaf/hank/monitor/notification/SQLNotificationFormatter.java
@@ -2,6 +2,11 @@ package com.rapleaf.hank.monitor.notification;
 
 import org.apache.log4j.Logger;
 
-public class SQLNotificationFormatter {
+public class SQLNotificationFormatter implements NotificationFormatter {
   private static Logger LOG = Logger.getLogger(SQLNotificationFormatter.class);
+
+  @Override
+  public String getWebUiLink(String absolutePath, String text) {
+    return text;
+  }
 }
--- src/java/com/rapleaf/hank/monitor/notifier/SQLNotifierFactory.java
+++ src/java/com/rapleaf/hank/monitor/notifier/SQLNotifierFactory.java
@@ -1,20 +1,45 @@
 package com.rapleaf.hank.monitor.notifier;
 
 import com.rapleaf.hank.config.InvalidConfigurationException;
+import com.rapleaf.hank.monitor.notification.StringNotification;
 import org.apache.log4j.Logger;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class SQLNotifierFactory extends AbstractNotifierFactory implements NotifierFactory {
   private static Logger LOG = Logger.getLogger(SQLNotifierFactory.class);
 
+  private static final String SQL_DRIVER = "sql_driver";
+  private static final String SQL_CONNECTION_URL = "sql_connection_url";
+  private static final String SQL_NOTIFICATION_TABLE = "sql_notification_table";
+
   @Override
   public void validate(Map<String, Object> configuration) throws InvalidConfigurationException {
-    //To change body of implemented methods use File | Settings | File Templates.
+    getRequiredString(configuration, SQL_DRIVER);
+    getRequiredString(configuration, SQL_CONNECTION_URL);
+    getRequiredString(configuration, SQL_NOTIFICATION_TABLE);
   }
 
   @Override
-  public Notifier createNotifier(Map<String, Object> configuration, String name, String webUiUrl) {
-    return null;  //To change body of implemented methods use File | Settings | File Templates.
+  public SQLNotifier createNotifier(Map<String, Object> configuration, String name, String webUiUrl) {
+    String driver = getString(configuration, SQL_DRIVER);
+    String url = getString(configuration, SQL_CONNECTION_URL);
+    String table = getString(configuration, SQL_NOTIFICATION_TABLE);
+
+    return new SQLNotifier(driver, url, table);
   }
+
+  public static void main(String arg[]) {
+    Map<String, Object> configuration = new HashMap<String, Object>() {{
+      put(SQL_DRIVER, "com.mysql.jdbc.Driver");
+      put(SQL_CONNECTION_URL, "jdbc:mysql://localhost/maindb_development?user=root&password=");
+      put(SQL_NOTIFICATION_TABLE, "hank_notification");
+    }};
+    String name = "test";
+    String url = "url";
+    SQLNotifier notifier = new SQLNotifierFactory().createNotifier(configuration, name, url);
+    notifier.doNotify(new StringNotification("test2"));
+  }
+
 }
