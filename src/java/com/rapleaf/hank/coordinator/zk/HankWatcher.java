abb7e04347ed38529fcea2f80356234a857d2373
Thomas Kielbus <thomas@rapleaf.com>
2012-03-17 12:45:51 -0700

    When not sync connected, do nothing in HankWatcher

--- src/java/com/rapleaf/hank/coordinator/zk/HankWatcher.java
+++ src/java/com/rapleaf/hank/coordinator/zk/HankWatcher.java
@@ -7,15 +7,15 @@ import org.apache.zookeeper.Watcher;
 
 /**
  * Abstract Watcher implementation that adds a few features.
- * 
+ * <p/>
  * The watch is immediately reset upon notification, *before* the subclass is
  * notified, guaranteeing that you won't miss notifications. Though, when you do
  * your get() call, you might get get data for events that happened after the
  * original notification, and then be notified *again* for the same data.
- * 
+ * <p/>
  * The process method is synchronized so that we don't have to worry about
  * concurrent notifications
- * 
+ * <p/>
  * Calling cancel() on the watcher sets a flag that prevents further
  * notifications and watch-setting.
  */
@@ -29,25 +29,30 @@ abstract class HankWatcher implements Watcher {
   }
 
   public abstract void setWatch() throws KeeperException, InterruptedException;
+
   protected abstract void realProcess(WatchedEvent event);
 
   @Override
   public final void process(WatchedEvent event) {
     synchronized (this) {
-      if (cancelled) {
-        return;
+      if (!cancelled) {
+        if (event.getState() == Event.KeeperState.SyncConnected) {
+          try {
+            setWatch();
+          } catch (KeeperException e) {
+            LOG.error("Failed to reset watch!", e);
+          } catch (InterruptedException e) {
+            // TODO: support retrying here?
+            LOG.error("Failed to reset watch!", e);
+          }
+          realProcess(event);
+        } else {
+          // Not sync connected, do nothing
+          if (LOG.isDebugEnabled()) {
+            LOG.debug("Not sync connected anymore for watched node.");
+          }
+        }
       }
-
-      try {
-        setWatch();
-      } catch (KeeperException e) {
-        LOG.error("Failed to reset watch!", e);
-      } catch (InterruptedException e) {
-        // TODO: support retrying here?
-        LOG.error("Failed to reset watch!", e);
-      }
-
-      realProcess(event);
     }
   }
 
