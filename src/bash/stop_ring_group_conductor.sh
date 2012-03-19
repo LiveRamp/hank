88ac395c1ea8135a6b38f39f4d26e5e08a339813
Thomas Kielbus <thomas@rapleaf.com>
2012-03-18 18:23:15 -0700

    When stopping processes, use kill -9

--- src/bash/stop_ring_group_conductor.sh
+++ src/bash/stop_ring_group_conductor.sh
@@ -1,4 +1,4 @@
 for PID in `jps -l | grep com.rapleaf.hank.ring_group_conductor.RingGroupConductor | cut -d ' ' -f 1`; do
   echo "Killing $PID"
-  kill $PID
+  kill -9 $PID
 done
--- src/bash/stop_web_ui.sh
+++ src/bash/stop_web_ui.sh
@@ -1,4 +1,4 @@
 for PID in `jps -l | grep com.rapleaf.hank.ui.WebUiServer | cut -d ' ' -f 1`; do
   echo "Killing $PID"
-  kill $PID
+  kill -9 $PID
 done
