f41f21fcc24b5920b2355df1fb68087f9cda435c
Thomas Kielbus <thomas@rapleaf.com>
2012-05-17 11:57:54 -0700

    Improve the HankSmartClientIface

--- src/java/com/rapleaf/hank/client/HankSmartClientIface.java
+++ src/java/com/rapleaf/hank/client/HankSmartClientIface.java
@@ -1,11 +1,18 @@
 package com.rapleaf.hank.client;
 
+import com.rapleaf.hank.generated.HankBulkResponse;
+import com.rapleaf.hank.generated.HankResponse;
 import com.rapleaf.hank.generated.SmartClient;
 import org.apache.thrift.TException;
 
 import java.nio.ByteBuffer;
+import java.util.List;
 
 public interface HankSmartClientIface extends SmartClient.Iface {
+
+  public HankResponse get(String domain_name, ByteBuffer key);
+
+  public HankBulkResponse getBulk(String domain_name, List<ByteBuffer> keys);
 
   public FutureGet concurrentGet(String domainName, ByteBuffer key) throws TException;
 
