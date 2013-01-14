546bd301916b3cf76b29aaab9ffb5df32076a034
Thomas Kielbus <thomas@liveramp.com>
2013-01-14 12:03:29 -0800

    Make version loading lazy, not static.

--- src/main/java/com/rapleaf/hank/Hank.java
+++ src/main/java/com/rapleaf/hank/Hank.java
@@ -1,60 +1,55 @@
 package com.rapleaf.hank;
 
-import org.apache.log4j.Logger;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
 public final class Hank {
-  private static final Logger LOG = Logger.getLogger(Hank.class);
 
   private Hank() {
   }
 
-  private final static String GIT_COMMIT;
+  private static String gitCommit = null;
 
-  static {
-
-    InputStream manifestStream = Hank.class.getResourceAsStream("META-INF/MANIFEST.MF");
-    try {
-      Manifest manifest = new Manifest(manifestStream);
-      Attributes attributes = manifest.getMainAttributes();
-      String temp = attributes.getValue("Implementation-Build");
-      if (temp != null) {
-        GIT_COMMIT = temp;
-      } else {
-        GIT_COMMIT = "Unknown";
+  public synchronized static String getGitCommit() {
+    if (gitCommit == null) {
+      InputStream manifestStream = Hank.class.getResourceAsStream("META-INF/MANIFEST.MF");
+      try {
+        Manifest manifest = new Manifest(manifestStream);
+        Attributes attributes = manifest.getMainAttributes();
+        String temp = attributes.getValue("Implementation-Build");
+        if (temp != null) {
+          gitCommit = temp;
+        } else {
+          gitCommit = "Unknown";
+        }
+      } catch (IOException ex) {
+        throw new RuntimeException(ex);
       }
-    } catch (IOException ex) {
-      throw new RuntimeException(ex);
     }
+    return gitCommit;
   }
 
-  public static String getGitCommit() {
-    return GIT_COMMIT;
-  }
-
-  private final static String VERSION;
-
-  static {
-    InputStream manifestStream = Hank.class.getResourceAsStream("META-INF/MANIFEST.MF");
-    try {
-      Manifest manifest = new Manifest(manifestStream);
-      Attributes attributes = manifest.getMainAttributes();
-      String temp = attributes.getValue("Implementation-Version");
-      if (temp != null) {
-        VERSION = temp;
-      } else {
-        VERSION = "Unknown";
-      }
-    } catch (IOException ex) {
-      throw new RuntimeException(ex);
-    }
-  }
+  private static String version = null;
 
   public static String getVersion() {
-    return VERSION;
+    if (version == null) {
+
+      InputStream manifestStream = Hank.class.getResourceAsStream("META-INF/MANIFEST.MF");
+      try {
+        Manifest manifest = new Manifest(manifestStream);
+        Attributes attributes = manifest.getMainAttributes();
+        String temp = attributes.getValue("Implementation-Version");
+        if (temp != null) {
+          version = temp;
+        } else {
+          version = "Unknown";
+        }
+      } catch (IOException ex) {
+        throw new RuntimeException(ex);
+      }
+    }
+    return version;
   }
 }
