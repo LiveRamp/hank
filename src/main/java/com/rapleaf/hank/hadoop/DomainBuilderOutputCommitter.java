e5274e2b1facede2dee7e8a00f4373725df7b1d4
Ben Podgursky <bpodgursky@liveramp.com>
2013-02-18 01:19:10 -0800

    ignore log and temporary directories when copying output stuff

--- src/main/java/com/rapleaf/hank/hadoop/DomainBuilderOutputCommitter.java
+++ src/main/java/com/rapleaf/hank/hadoop/DomainBuilderOutputCommitter.java
@@ -25,10 +25,19 @@ import org.apache.hadoop.mapred.JobContext;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class DomainBuilderOutputCommitter extends FileOutputCommitter {
 
   private static final Logger LOG = Logger.getLogger(DomainBuilderOutputCommitter.class);
+
+  private static final Set<String> IGNORE_PATHS = new HashSet<String>(Arrays.asList(
+      "_logs",
+      "_temporary"
+  ));
 
   // Note: setupJob() commitJob() and cleanupJob() should get called automatically by the MapReduce
   // framework in subsequent versions of Hadoop.
@@ -65,19 +74,21 @@ public class DomainBuilderOutputCommitter extends FileOutputCommitter {
     LOG.info("Moving temporary output files from: " + tmpOutputPath + " to final output path: " + outputPath);
     FileStatus[] partitions = fs.listStatus(tmpOutputPath);
     for (FileStatus partition : partitions) {
-      if (partition.isDir()) {
-        FileStatus[] partitionFiles = fs.listStatus(partition.getPath());
-        for (FileStatus partitionFile : partitionFiles) {
-          Path sourcePath = partitionFile.getPath();
-          Path targetPath = new Path(new Path(outputPath, partition.getPath().getName()), partitionFile.getPath().getName());
-          if (LOG.isDebugEnabled()) {
-            LOG.debug("Moving: " + sourcePath + " to: " + targetPath);
-          }
-          if (!fs.mkdirs(targetPath.getParent())) {
-            throw new IOException("Failed at creating directory " + targetPath.getParent());
-          }
-          if (!fs.rename(sourcePath, targetPath)) {
-            throw new IOException("Failed at renaming " + sourcePath + " to " + targetPath);
+      if(!IGNORE_PATHS.contains(partition.getPath().getName())){
+        if (partition.isDir()) {
+          FileStatus[] partitionFiles = fs.listStatus(partition.getPath());
+          for (FileStatus partitionFile : partitionFiles) {
+            Path sourcePath = partitionFile.getPath();
+            Path targetPath = new Path(new Path(outputPath, partition.getPath().getName()), partitionFile.getPath().getName());
+            if (LOG.isDebugEnabled()) {
+              LOG.debug("Moving: " + sourcePath + " to: " + targetPath);
+            }
+            if (!fs.mkdirs(targetPath.getParent())) {
+              throw new IOException("Failed at creating directory " + targetPath.getParent());
+            }
+            if (!fs.rename(sourcePath, targetPath)) {
+              throw new IOException("Failed at renaming " + sourcePath + " to " + targetPath);
+            }
           }
         }
       }
