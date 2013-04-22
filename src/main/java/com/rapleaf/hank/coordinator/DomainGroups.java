9a5606f201c95e2001b69d3de8eddecb28764521
Porter Westling <pwestling@liveramp.com>
2013-04-22 15:05:55 -0700

    cleanup

--- src/main/java/com/rapleaf/hank/coordinator/DomainGroups.java
+++ src/main/java/com/rapleaf/hank/coordinator/DomainGroups.java
@@ -1,35 +1,32 @@
 package com.rapleaf.hank.coordinator;
 
 import com.google.common.collect.Maps;
-import org.apache.log4j.Logger;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Map;
-import java.util.Set;
 
 public class DomainGroups {
-
-  private static final Logger LOG = Logger.getLogger(DomainGroups.class);
 
   private DomainGroups() {
   }
 
 
-  public static void fastForwardDomain(DomainGroup group, Domain domain) throws IOException {
+  public static void fastForwardDomain(DomainGroup domainGroup, Domain domain) throws IOException {
     DomainVersion version = Domains.getLatestVersionNotOpenNotDefunct(domain);
-    group.setDomainVersion(domain, version.getVersionNumber());
+    domainGroup.setDomainVersion(domain, version.getVersionNumber());
   }
 
-  public static void fastForwardDomains(DomainGroup group, Set<Domain> domains) throws IOException {
+  public static void fastForwardDomains(DomainGroup domainGroup, Collection<Domain> domains) throws IOException {
     Map<Domain, Integer> domainVersions = Maps.newHashMap();
     for (Domain domain : domains) {
       domainVersions.put(domain, Domains.getLatestVersionNotOpenNotDefunct(domain).getVersionNumber());
     }
-    group.mergeDomainVersions(domainVersions);
+    domainGroup.mergeDomainVersions(domainVersions);
   }
 
-  public static void fastForwardAllDomains(DomainGroup group) throws IOException {
-    fastForwardDomains(group, group.getDomains());
+  public static void fastForwardAllDomains(DomainGroup domainGroup) throws IOException {
+    fastForwardDomains(domainGroup, domainGroup.getDomains());
   }
 
 }
