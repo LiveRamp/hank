55580560c3874853ab2cff5beb60c19a849c8653
Thomas Kielbus <thomas@liveramp.com>
2013-04-22 18:26:18 -0700

    Change license copyright to LiveRamp

--- hank-core/src/main/java/com/rapleaf/hank/partition_server/RuntimeStatisticsAggregator.java
+++ hank-core/src/main/java/com/rapleaf/hank/partition_server/RuntimeStatisticsAggregator.java
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2011 Rapleaf
+ *  Copyright 2011 LiveRamp
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
