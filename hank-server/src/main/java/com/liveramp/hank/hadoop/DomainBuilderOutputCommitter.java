f86c8995fbaf39890d4dc51ea02afc2b1ecbfa17
Matt Hagy <matt@liveramp.com>
2013-12-02 16:45:51 -0800

    Reverted back to old imports

--- hank-server/src/main/java/com/liveramp/hank/hadoop/DomainBuilderOutputCommitter.java
+++ hank-server/src/main/java/com/liveramp/hank/hadoop/DomainBuilderOutputCommitter.java
@@ -26,7 +26,10 @@ import org.apache.hadoop.mapred.JobContext;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
