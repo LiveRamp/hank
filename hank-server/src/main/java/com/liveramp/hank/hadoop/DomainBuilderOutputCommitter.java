764ba02cc42a4543a0785915a184adc18c33bb3d
Matt Hagy <matt@liveramp.com>
2013-12-02 18:04:22 -0800

    Fixed typo

--- hank-server/src/main/java/com/liveramp/hank/hadoop/DomainBuilderOutputCommitter.java
+++ hank-server/src/main/java/com/liveramp/hank/hadoop/DomainBuilderOutputCommitter.java
@@ -30,6 +30,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.ArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
