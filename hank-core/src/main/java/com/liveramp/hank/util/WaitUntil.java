14304b29f5f095758d6bd916f0464da7cc9cae86
Thomas Kielbus <thomas@liveramp.com>
2014-06-12 17:33:08 -0700

    Add WaitUntil.orReturn()

--- hank-core/src/main/java/com/liveramp/hank/util/WaitUntil.java
+++ hank-core/src/main/java/com/liveramp/hank/util/WaitUntil.java
@@ -25,6 +25,21 @@ public class WaitUntil {
     }
   }
 
+  public static boolean orReturn(Condition condition) throws InterruptedException {
+    return orReturn(condition, ExponentialBackoff.DEFAULT_MAXIMUM_BACKOFF_MS);
+  }
+
+  public static boolean orReturn(Condition condition, long maximumBackoffMs) throws InterruptedException {
+    ExponentialBackoff exponentialBackoff = new ExponentialBackoff(ExponentialBackoff.DEFAULT_INITIAL_BACKOFF_MS, maximumBackoffMs);
+    while (!condition.test()) {
+      if (exponentialBackoff.isMaxedOut()) {
+        return false;
+      }
+      exponentialBackoff.backoff();
+    }
+    return true;
+  }
+
   public static void orDie(Condition condition) throws InterruptedException {
     orDie(condition, ExponentialBackoff.DEFAULT_MAXIMUM_BACKOFF_MS);
   }
