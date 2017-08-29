1e083281b2eefa7f80863df14e1495557248409e
Ben Podgursky <bpodgursky@liveramp.com>
2017-08-29 10:52:20 -0700

    synchronize on serializear

--- hank-ui/src/main/java/com/liveramp/hank/ui/HankApiHelper.java
+++ hank-ui/src/main/java/com/liveramp/hank/ui/HankApiHelper.java
@@ -1,17 +1,17 @@
 /**
- *  Copyright 2013 LiveRamp
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright 2013 LiveRamp
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.liveramp.hank.ui;
@@ -230,12 +230,14 @@ public class HankApiHelper {
     RuntimeStatisticsSummary summary = host.getRuntimeStatisticsSummary();
 
     //  just booting up, not deployed, etc
-    if(summary == null){
+    if (summary == null) {
       return new JSONObject().toString();
     }
 
     try {
-      return JSON_SERIALIZER.toString(summary);
+      synchronized (JSON_SERIALIZER) {
+        return JSON_SERIALIZER.toString(summary);
+      }
     } catch (TException e) {
       throw new IOException(e);
     }
