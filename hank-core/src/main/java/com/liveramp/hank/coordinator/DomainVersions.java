a723d4ee4bfea07bf5340bfbc0c5aef100717552
Ben Podgursky <bpodgursky@liveramp.com>
2018-02-12 19:38:35 -0800

    don't give the option of deleting a domain version which is targeted by domain group

--- hank-core/src/main/java/com/liveramp/hank/coordinator/DomainVersions.java
+++ hank-core/src/main/java/com/liveramp/hank/coordinator/DomainVersions.java
@@ -1,17 +1,17 @@
 /**
- *  Copyright 2011 LiveRamp
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright 2011 LiveRamp
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.liveramp.hank.coordinator;
@@ -26,6 +26,21 @@ public final class DomainVersions {
 
   public static boolean isClosed(DomainVersion domainVersion) throws IOException {
     return domainVersion.getClosedAt() != null;
+  }
+
+  public static boolean isCurrentlyServed(Domain domain, DomainVersion domainVersion, Coordinator coord) throws IOException {
+
+    for (DomainGroup domainGroup : coord.getDomainGroups()) {
+      DomainAndVersion version = domainGroup.getDomainVersion(domain);
+
+      if (version != null) {
+        if (version.getVersionNumber() == domainVersion.getVersionNumber()) {
+          return true;
+        }
+      }
+    }
+
+    return false;
   }
 
   public static long getTotalNumBytes(DomainVersion domainVersion) throws IOException {
